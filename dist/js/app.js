0de04c459a68950acb987c61dce18649fa923369
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2021-05-20 21:19:22 +1000

    Add submit functionality for contact form

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,7 +11,7 @@ window.addEventListener("scroll", resizeNav);
 const typedTextOptions = ["developer.", "student.", "React.js enthusiast."];
 const typingDelay = 100;
 const erasingDelay = 100;
-const newTextDelay = 1500; // Delay between current and next text
+const newTextDelay = 1500;
 let textArrayIndex = 0;
 let charIndex = 0;
 
@@ -77,4 +77,47 @@ function resizeNav() {
     logoName.classList.remove("text-white");
     logoName.classList.add("text-blue");
   }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+  var form = document.getElementById("contact-form");
+
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
