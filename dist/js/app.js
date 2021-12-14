9cbf264b4f9023dcce92ad39b4f1a550f81b984b
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2021-12-14 16:54:50 +1000

    Delete redundant code

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,61 +1,14 @@
 const logoName = document.getElementById("logo-name");
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
 const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 const body = document.querySelector("body");
 const arrow = document.getElementById("arrow-scroll");
 const navbar = document.querySelector("nav");
 
-const typedTextOptions = ["developer.", "student."];
-const typingDelay = 100;
-const erasingDelay = 100;
-const newTextDelay = 1500;
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < typedTextOptions[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent +=
-      typedTextOptions[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = typedTextOptions[textArrayIndex].substring(
-      0,
-      charIndex - 1
-    );
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= typedTextOptions.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // On DOM Load initiate the effect
-  if (typedTextOptions.length) setTimeout(type, newTextDelay + 250);
-});
-
 window.addEventListener("DOMContentLoaded", function () {
-  // get the form elements defined in your form HTML above
+  // Get the form elements defined in your form HTML above
   var form = document.getElementById("contact-form");
 
-  // var button = document.getElementById("my-form-button");
   var status = document.getElementById("status");
 
   // Success and Error functions for after the form is submitted
@@ -70,7 +23,7 @@ window.addEventListener("DOMContentLoaded", function () {
     status.innerHTML = "Oops! There was a problem.";
   }
 
-  // handle the form submission event
+  // Handle the form submission event
   form.addEventListener("submit", function (ev) {
     ev.preventDefault();
     var data = new FormData(form);
@@ -78,7 +31,7 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// helper function for sending an AJAX request
+// Helper function for sending an AJAX request
 function ajax(method, url, data, success, error) {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url);
