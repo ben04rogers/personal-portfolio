72cc08362ced764d333e266f228fbaaf00edce07
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-01-12 12:50:34 +1000

    Paper plane animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,8 +11,8 @@ window.addEventListener("DOMContentLoaded", function () {
   // Success and Error functions for after the form is submitted
   function success() {
     form.reset();
-    status.classList.add("success");
-    status.innerHTML = "Email sent successfully!";
+    // status.classList.add("success");
+    // status.innerHTML = "Email sent successfully!";
   }
 
   function error() {
@@ -37,6 +37,139 @@ function ajax(method, url, data, success, error) {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
     if (xhr.status === 200) {
       success(xhr.response, xhr.responseType);
+
+      // Paper plane animation
+      // Credit: https://codepen.io/aaroniker/pen/BajabVN
+      document.querySelectorAll(".btn-email").forEach((button) => {
+        let getVar = (variable) =>
+          getComputedStyle(button).getPropertyValue(variable);
+
+        if (!button.classList.contains("active")) {
+          button.classList.add("active");
+
+          gsap.to(button, {
+            keyframes: [
+              {
+                "--left-wing-first-x": 50,
+                "--left-wing-first-y": 100,
+                "--right-wing-second-x": 50,
+                "--right-wing-second-y": 100,
+                duration: 0.2,
+                onComplete() {
+                  gsap.set(button, {
+                    "--left-wing-first-y": 0,
+                    "--left-wing-second-x": 40,
+                    "--left-wing-second-y": 100,
+                    "--left-wing-third-x": 0,
+                    "--left-wing-third-y": 100,
+                    "--left-body-third-x": 40,
+                    "--right-wing-first-x": 50,
+                    "--right-wing-first-y": 0,
+                    "--right-wing-second-x": 60,
+                    "--right-wing-second-y": 100,
+                    "--right-wing-third-x": 100,
+                    "--right-wing-third-y": 100,
+                    "--right-body-third-x": 60,
+                  });
+                },
+              },
+              {
+                "--left-wing-third-x": 20,
+                "--left-wing-third-y": 90,
+                "--left-wing-second-y": 90,
+                "--left-body-third-y": 90,
+                "--right-wing-third-x": 80,
+                "--right-wing-third-y": 90,
+                "--right-body-third-y": 90,
+                "--right-wing-second-y": 90,
+                duration: 0.2,
+              },
+              {
+                "--rotate": 50,
+                "--left-wing-third-y": 95,
+                "--left-wing-third-x": 27,
+                "--right-body-third-x": 45,
+                "--right-wing-second-x": 45,
+                "--right-wing-third-x": 60,
+                "--right-wing-third-y": 83,
+                duration: 0.25,
+              },
+              {
+                "--rotate": 60,
+                "--plane-x": -8,
+                "--plane-y": 40,
+                duration: 0.2,
+              },
+              {
+                "--rotate": 40,
+                "--plane-x": 45,
+                "--plane-y": -300,
+                "--plane-opacity": 0,
+                duration: 0.375,
+                onComplete() {
+                  setTimeout(() => {
+                    button.removeAttribute("style");
+                    gsap.fromTo(
+                      button,
+                      {
+                        opacity: 0,
+                        y: -8,
+                      },
+                      {
+                        opacity: 1,
+                        y: 0,
+                        clearProps: true,
+                        duration: 0.3,
+                        onComplete() {
+                          button.classList.remove("active");
+                        },
+                      }
+                    );
+                  }, 1800);
+                },
+              },
+            ],
+          });
+
+          gsap.to(button, {
+            keyframes: [
+              {
+                "--text-opacity": 0,
+                "--border-radius": 0,
+                "--left-wing-background": getVar("--primary-dark"),
+                "--right-wing-background": getVar("--primary-dark"),
+                duration: 0.11,
+              },
+              {
+                "--left-wing-background": getVar("--primary"),
+                "--right-wing-background": getVar("--primary"),
+                duration: 0.14,
+              },
+              {
+                "--left-body-background": getVar("--primary-dark"),
+                "--right-body-background": getVar("--primary-darkest"),
+                duration: 0.25,
+                delay: 0.1,
+              },
+              {
+                "--trails-stroke": 171,
+                duration: 0.22,
+                delay: 0.22,
+              },
+              {
+                "--success-opacity": 1,
+                "--success-x": 0,
+                duration: 0.2,
+                delay: 0.15,
+              },
+              {
+                "--success-stroke": 0,
+                duration: 0.15,
+              },
+            ],
+          });
+        }
+      });
     } else {
       error(xhr.status, xhr.response, xhr.responseType);
     }
@@ -60,3 +193,5 @@ backToTopBtn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+// Email button
